79a729868c49f6dfae5b9a008c5ab54187dcb231
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-23 22:11:56 +0530

    create : firebase/config.js
    - intialised firebase

---
+++ client/src/firebase/config.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+	apiKey: "AIzaSyCfmQYjkCoA6D5HgcGyFKCECi7q13C0F4k",
+	authDomain: "picbook-ad888.firebaseapp.com",
+	projectId: "picbook-ad888",
+	storageBucket: "picbook-ad888.appspot.com",
+	messagingSenderId: "393588963750",
+	appId: "1:393588963750:web:5b730cd0aa40d3775ef767",
+	measurementId: "G-QFTMJYB29X",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
