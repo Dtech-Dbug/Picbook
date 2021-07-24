d5753f70d9a408e15f378f6ab5dc83b8c24c0470
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-24 12:40:20 +0530

    update : progressBar.js
    - showed progressbar
    - removed progress bar by setting file to null again when the url is ready

--- client/src/Components/ProgressBar.js
+++ client/src/Components/ProgressBar.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStorage from "../Hooks/useStrorage";
 
 const ProgressBar = ({ file, setFile }) => {
 	const { url, progress } = useStorage(file);
-	console.log(progress, url);
+
+	//console.log(progress, url);
+	//how to remove the progress bar once fileis uploaded?
+	// set file to null again, bcz progress bar fires when there is file
+	//we can setFile t null once we get the Image URL
+
+	useEffect(() => {
+		if (url) setFile(null);
+	}, [url, setFile]);
+
 	return <div className="progress-bar" style={{ width: progress + "%" }}></div>;
 };
 
