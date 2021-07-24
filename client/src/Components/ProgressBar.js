8cfe03a372b8c19b23999216694fd1902150cbf1
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-24 11:52:38 +0530

    create : progressBar.js
    - dummy component for testing. Working

---
+++ client/src/Components/ProgressBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import useStrorage from "../Hooks/useStrorage";
+
+const ProgressBar = () => {
+	return (
+		<div>
+			<p>progress</p>
+		</div>
+	);
+};
+
+export default ProgressBar;
