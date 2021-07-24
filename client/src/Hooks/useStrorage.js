d9cafcd597b83d3827ebdaa14bd4d04c5c3b0533
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-24 11:38:43 +0530

    create : useStorage.js
    - custom hook to handle al the fire firebase upload logic

---
+++ client/src/Hooks/useStrorage.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { projectStorage } from "../firebase/config";
+
+//creating a custom hook
+//resposnible to handle file uploads in firebase
+
+const useStrorage = (file) => {
+	const [error, setError] = useState(null);
+	const [progress, setProgress] = useState(0);
+	const [url, setUrl] = useState(null);
+
+	useEffect(() => {
+		//referance to a file inside firebase storage
+		let storageRef = projectStorage.ref(file.name);
+
+		//use put method to upload the file in the refrenace location
+		//async function, whhc will trigger sevral events
+		//we can take care of those events
+
+		//.on is a event listener which takes a cpuple of args
+		// arg1 : the event
+		//...rest args : callbacks
+
+		storageRef.put(file).on(
+			"state_changed",
+			(snap) => {
+				let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+				setProgress(percentage);
+			},
+			(err) => {
+				setError(err);
+			},
+			async () => {
+				const url = await storageRef.getDownloadURL();
+				setUrl(url);
+			}
+		);
+	}, [file]);
+
+	return { progress, url, error };
+};
+
+export default useStrorage;
