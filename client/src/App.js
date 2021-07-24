9a6b0bbe422e7722f09623ea78f7178faccbf335
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-24 20:40:31 +0530

    update : App.js => imported new component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
+import { useState } from "react/cjs/react.development";
+import ImageGrid from "./Components/ImageGrid";
+import Modal from "./Components/Modal";
 import UploadFile from "./Components/UploadFile";
 
 function App() {
+	const [selectedImage, setSelectedImage] = useState(null);
 	return (
 		<div className="App">
 			<h1>PicBook</h1>
 			<UploadFile />
+			<ImageGrid
+				selectedImage={selectedImage}
+				setSelectedImage={setSelectedImage}
+			/>
+			<Modal selectedImage={selectedImage} />
 		</div>
 	);
 }
