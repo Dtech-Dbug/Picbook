844f019a416e41cd5d03e625afb7fe68b0b0efea
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-24 11:03:39 +0530

    create : uploadFole.js component
    - to upload images
    - check image type
    - setError if there is conflict

---
+++ client/src/Components/UploadFile.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const UploadFile = () => {
+	const [file, setFile] = useState(null);
+	const [error, setError] = useState(null);
+	const imageTypes = ["image/png", "image/jpeg"];
+
+	const handleFileUpload = (e) => {
+		const uploadedFile = e.target.files[0];
+
+		if (uploadedFile && imageTypes.includes(uploadedFile.type)) {
+			console.log(uploadedFile);
+			//set error to null, if the file type is roght
+			setError(null);
+
+			//store the seleted file in a local state
+			setFile(uploadedFile);
+		} else {
+			console.log("No");
+			setError("Invalid File Format. Please Check");
+		}
+	};
+	return (
+		<form>
+			<input type="file" onChange={handleFileUpload}></input>
+			<br />
+			{error}
+		</form>
+	);
+};
+
+export default UploadFile;
